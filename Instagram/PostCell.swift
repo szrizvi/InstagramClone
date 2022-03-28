facd4b51f1f4f49884061ae2f44ed5de42c47c8e
szrizvi <77036574+szrizvi@users.noreply.github.com>
2022-03-27 22:51:21 -0700

    required stories

---
+++ Instagram/PostCell.swift
@@ -0,0 +1,26 @@
+//
+//  PostCell.swift
+//  Instagram
+//
+//  Created by Zainab Rizvi on 26/03/2022.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var photoView: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var captionLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
